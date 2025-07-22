64e004a6c431332d8dddae68fdd6b61460238a47
Jesse Kopala <jdkopala@gmail.com>
2025-07-22 14:34:47 -0600

    clean up stage script

--- Scripts/stage.gd
+++ Scripts/stage.gd
@@ -4,6 +4,12 @@ extends Node2D
 @onready var green_blocks = $GreenBlocks.get_children()
 @onready var blue_blocks = $BlueBlocks.get_children()
 
+@onready var block_groups = {
+	0: $RedBlocks.get_children(),
+	1: $GreenBlocks.get_children(),
+	2: $BlueBlocks.get_children()
+}
+
 @onready var player: Node2D = $"../Player"
 
 func _ready():
@@ -17,31 +23,14 @@ func _ready():
 		
 # When the current dino is removed
 func _on_player_child_exiting_tree(_dnode: Node) -> void:
-	if player.current_color == 0:
-		for block in red_blocks:
-			disable_block(block)
-	if player.current_color == 1:
-		for block in green_blocks:
-			disable_block(block)
-	if player.current_color == 2:
-		for block in blue_blocks:
-			disable_block(block)
+	for block in block_groups[player.current_color]:
+		disable_block(block)
 
 # When the dino is replaced
 func _on_player_child_entered_tree(_node: Node) -> void:
 	if player:
-		# Changed to Red dino
-		if player.current_color == 0:
-			for block in red_blocks:
-				enable_block(block)
-		# Changed to Green dino
-		if player.current_color == 1:
-			for block in green_blocks:
-				enable_block(block)
-		# Changed to Blue Dino
-		if player.current_color == 2:
-			for block in blue_blocks:
-				enable_block(block)
+		for block in block_groups[player.current_color]:
+			enable_block(block)
 
 func disable_block(block: RigidBody2D):
 	# Disable collision box
