45cb0578ffb545f28f293f92d7fb3605e935acd9
Jesse Kopala <jdkopala@gmail.com>
2025-07-22 14:02:54 -0600

    Scenes/

--- Scripts/stage.gd
+++ Scripts/stage.gd
@@ -9,24 +9,23 @@ extends Node2D
 func _ready():
 	# We will always start on red
 	for block in red_blocks:
-		disable_block(block)
+		enable_block(block)
 	for block in green_blocks:
-		enable_block(block)
+		disable_block(block)
 	for block in blue_blocks:
-		enable_block(block)
+		disable_block(block)
 		
 # When the current dino is removed
 func _on_player_child_exiting_tree(_dnode: Node) -> void:
-	if player:
-		if player.current_color == 0:
-			for block in red_blocks:
-				enable_block(block)
-		if player.current_color == 1:
-			for block in green_blocks:
-				enable_block(block)
-		if player.current_color == 2:
-			for block in blue_blocks:
-				enable_block(block)
+	if player.current_color == 0:
+		for block in red_blocks:
+			disable_block(block)
+	if player.current_color == 1:
+		for block in green_blocks:
+			disable_block(block)
+	if player.current_color == 2:
+		for block in blue_blocks:
+			disable_block(block)
 
 # When the dino is replaced
 func _on_player_child_entered_tree(_node: Node) -> void:
@@ -34,15 +33,15 @@ func _on_player_child_entered_tree(_node: Node) -> void:
 		# Changed to Red dino
 		if player.current_color == 0:
 			for block in red_blocks:
-				disable_block(block)
+				enable_block(block)
 		# Changed to Green dino
 		if player.current_color == 1:
 			for block in green_blocks:
-				disable_block(block)
+				enable_block(block)
 		# Changed to Blue Dino
 		if player.current_color == 2:
 			for block in blue_blocks:
-				disable_block(block)
+				enable_block(block)
 
 func disable_block(block: RigidBody2D):
 	# Disable collision box
