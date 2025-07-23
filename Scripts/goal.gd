10729477d5da0062450b5e6fc45b8f242cdc6299
Jesse Kopala <jdkopala@gmail.com>
2025-07-23 11:04:55 -0600

    play audio on reaching goal, change sprites

--- Scripts/goal.gd
+++ Scripts/goal.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
 @onready var sprite: AnimatedSprite2D = $Sprite
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var victory_sound: AudioStream = preload("res://Assets/Sounds/Level Up 2 (miniclip).ogg")
 
 func level_complete_animate():
+	play_audio(victory_sound)
 	sprite.animation = "Crack"
 	sprite.animation = "Hatch"
+
+func play_audio(stream: AudioStream):
+	audio.stream = stream
+	audio.play()
